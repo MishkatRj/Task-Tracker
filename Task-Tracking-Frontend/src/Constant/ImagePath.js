6197c6938050bdc431957f9dbde05fd0f34dbf20
Muhammad Hamza <MuhammadHamza524727@users.noreply.github.com>
2025-04-27 21:50:56 +0500

    any commit

--- Task-Tracking-Frontend/src/Constant/ImagePath.js
+++ Task-Tracking-Frontend/src/Constant/ImagePath.js
@@ -1,4 +1,4 @@
 export { default as logo } from "../Assets/Images/logo.png";
-export { default as login } from "../Assets/Images/login.jpg";
-export { default as signup } from "../Assets/Images/login.jpg";
-export { default as Logoo } from "../Assets/Images/Logoo.png";
+export { default as login } from "../Assets/Images/logo1.jpg";
+export { default as signup } from "../Assets/Images/logo1.jpg";
+export { default as Logoo } from "../Assets/Images/logo22.png";
